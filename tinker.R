2358d3df1be2a2a243b938710264751832ea3af7
Conor Miles <conormgm@gmail.com>
2018-12-03 14:01:58 -0800

    modify tinker, examine data

--- tinker.R
+++ tinker.R
@@ -1,1 +1,11 @@
+library(httr)
+library(lubridate)
+library(dplyr)
+library(jsonlite)
 
+address <- "https://earthquake.usgs.gov/fdsnws/event/1/" 
+method <- "query?"
+format <- "format=csv"
+start_and_end <- "starttime=2014-01-01&endtime=2014-01-02" 
+usgs_uri <- paste0(address, method, format, "&", start_and_end)
+usgs_res <- GET(usgs_uri)
