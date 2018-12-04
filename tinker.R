11b0801ff990c751b79362275353bb034c00c126
Larry Gao <lpgao25@gmail.com>
2018-12-03 19:01:20 -0800

    created dataframe in tinker

--- tinker.R
+++ tinker.R
@@ -9,3 +9,8 @@ format <- "format=csv"
 start_and_end <- "starttime=2014-01-01&endtime=2014-01-02" 
 usgs_uri <- paste0(address, method, format, "&", start_and_end)
 usgs_res <- GET(usgs_uri)
+
+usgs_body <- content(usgs_res, "text")
+usgs_parsed_data <- fromJSON(usgs_body)
+usgs_features <- data.frame(usgs_parsed_data$features)
+usgs_data_frame <- flatten(usgs_features)
