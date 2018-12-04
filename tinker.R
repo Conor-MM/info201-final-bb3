179a2f3ba0e286716fdb36fe2b27fb0467911474
Larry Gao <lpgao25@gmail.com>
2018-12-03 18:05:00 -0800

    edited tinker with json

--- tinker.R
+++ tinker.R
@@ -5,7 +5,10 @@ library(jsonlite)
 
 address <- "https://earthquake.usgs.gov/fdsnws/event/1/" 
 method <- "query?"
-format <- "format=csv"
+format <- "format=geojson"
 start_and_end <- "starttime=2014-01-01&endtime=2014-01-02" 
 usgs_uri <- paste0(address, method, format, "&", start_and_end)
 usgs_res <- GET(usgs_uri)
+
+usgs_body <- content(usgs_res, "text")
+usgs_parsed_data <- fromJSON(body)
