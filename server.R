30339acb61b0250d0cefe44fec7e0fe82d6bed2f
lp36 <lp36@uw.edu>
2018-12-04 22:40:30 -0800

    Updated summary & added zoom in on state

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ library(lubridate)
 library(dplyr)
 library(jsonlite)
 library(ggplot2)
+library(stringr)
 
 server <- function(input, output) {
 
@@ -20,8 +21,18 @@ server <- function(input, output) {
   })
 
   output$summary <- renderText({
-    sprintf("From %s to %s, there has been a total of %s earthquakes in the selected region, averaging %s per day. The average magnitude is %s and the highest is %s.",
-    input$date_range[1], input$date_range[2], values$count, round(values$count/as.numeric((date1 - date2), units = "days"), 2), values$mean, values$max)
+    shiny::validate(
+       need(input$date_range, "Just one sec...")
+    )
+    result <- sprintf("From %s to %s, there has been a total of %s earthquakes in %s.",
+    input$date_range[1], input$date_range[2], values$count, values$region
+    )
+    if(!values$empty){
+      result <- paste(result, sprintf("Averaging %s per day over %s days. The average magnitude is %s and the highest is %s.",
+      round(values$count/as.numeric((input$date_range[2] - input$date_range[1]), units = "days"), 2), (input$date_range[2] - input$date_range[1]),
+      values$mean, values$max))
+    }
+    return(result)
   })
 
 
@@ -33,20 +44,27 @@ server <- function(input, output) {
     usgs_res <- GET(usgs_uri)
 
     usgs_data <- content(usgs_res, "parsed")[, c(1,2,3,5,14)]
-    if(input$states != "Unspecified"){
-      selected_states <- c(input$states, state.abb[match(input$states, state.name)])
+    if(input$states == "Unspecified"){
+      us_states <- c(state.name, state.abb)
+      geo_data <- map_data("state")
+      filtered_data <- usgs_data %>%
+        filter(str_detect(paste(us_states, collapse="|"), (word(place, -1)))) %>%
+        filter(longitude > -124 & longitude < -67 & latitude > 25 & latitude < 50)
+      values$region <- "United States(Excluding Alaska and Hawaii)"
     } else{
-      selected_states <- c(state.name, state.abb)
-    }
+    selected_state <- c(input$states, state.abb[match(input$states, state.name)])
+    geo_data <- filter(map_data("state"), region == tolower(input$states))
     filtered_data <- usgs_data %>%
-      filter(str_detect(paste(selected_states, collapse="|"), (word(place, -1)))) %>%
-      filter(longitude > -124 & longitude < -67 & latitude > 25 & latitude < 50)
+      filter(str_detect(paste(selected_state, collapse="|"), (word(place, -1))))
+      values$region <- input$states
+    }
 
     values$count <- nrow(filtered_data)
     values$mean <- round(mean(filtered_data$mag, na.rm=TRUE), 2)
     values$max <- max(filtered_data$mag, na.rm=TRUE)
+    values$empty <- dim(filtered_data)[1] == 0
 
-    usgs_map <- ggplot() + geom_polygon(data=map_data("state"), aes(x=long, y=lat, group = group),colour="black", fill="white") +
+    usgs_map <- ggplot() + geom_polygon(data=geo_data, aes(x=long, y=lat, group = group),colour="black", fill="white") +
     geom_point(filtered_data, mapping = aes(longitude, latitude), color = "red", size = filtered_data$mag) + coord_quickmap()
     return(usgs_map)
   })
